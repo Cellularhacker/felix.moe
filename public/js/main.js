09f00c2d419859e53f333b96b0a53eb6e6835e8c
Cellularhacker <wva3cdae@gmail.com>
2018-07-22 18:27:14 +0900

    Now, the main page will show random-image from the array.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,21 @@
+// Button for Copying Text to  Clipboard 
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
+}
+
+// Loading Random Felix Image
+var imageURLs = [
+    "http://felix.moe/images/maxresdefault.jpg",
+    "http://felix.moe/images/maxresdefault2.jpg"
+];
+function getImageTag() {
+    var img= '<img src=\"';
+    var randomIndex = Math.floor(Math.random() * imageURLs.length);
+    img += imageURLs[randomIndex];
+    img += '\" alt="Felix Magi Tenshi!\"  width=\"70%\">';
+    return img;
 }
